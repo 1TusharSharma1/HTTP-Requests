32957beea534a4ed7ed56d37dd5e7ac13dc8e9af
Tushar <tush.sharma2504@gmail.com>
2023-05-30 00:43:22 +0530

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     try {
       setLoading(true);
       setError(null);
-      const response = await fetch("https://swapi.dev/api/film");
+      const response = await fetch("https://swapi.dev/api/films");
       if (!response.ok) {
         throw new Error("Something Went Wrong!");
       }
